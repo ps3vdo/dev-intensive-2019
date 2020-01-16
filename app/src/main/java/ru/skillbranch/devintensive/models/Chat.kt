33397611694ca60afcc3a973d684ad9bcbca6b80
ps3vdo <nikeron-alfa@ya.ru>
2020-01-16 20:30:05 +0300

    na proverku

--- app/src/main/java/ru/skillbranch/devintensive/models/Chat.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/Chat.kt
@@ -3,7 +3,7 @@ package ru.skillbranch.devintensive.models
 class Chat(
     val id:String,
     val members: MutableList<User> = mutableListOf(),
-    val messages: MutableList<BaseMassage> = mutableListOf()
+    val messages: MutableList<BaseMessage> = mutableListOf()
 
 ) {
 }
